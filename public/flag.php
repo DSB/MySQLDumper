76b2553bf2ca4ab3b5e62d44eba07b78cc898e04
DSB <admin@mysqldumper.de>
2011-07-03 19:41:35 +0000

    Added check if GET['langId'] exists

--- public/flag.php
+++ public/flag.php
@@ -1,10 +1,10 @@
 <?php
-$country = $_GET['langId'];
+$language = isset($_GET['langId']) ? $_GET['langId'] : '';
 
-if (preg_match('/^[A-Z0-9_]+\z/i', $country)) {
+if (preg_match('/^[A-Z0-9_]+\z/i', $language)) {
     define('DS', DIRECTORY_SEPARATOR);
     $languageDir = realpath(dirname(__FILE__) . '/../application/language');
-    $image = $languageDir . DS . $country . DS . 'flag.gif';
+    $image = $languageDir . DS . $language . DS . 'flag.gif';
 
     if (file_exists($image)) {
         header('Content-Type: image/gif');
