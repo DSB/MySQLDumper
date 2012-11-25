82085eda1609c64d79a67fd91466da91e5eead3f
translationcenter <translationcenter@mysqldumper.de>
2012-11-25 04:24:53 -0800

    Language pack update.

--- application/language/de/lang.php
+++ application/language/de/lang.php
@@ -7,10 +7,10 @@
  * @subpackage    Languages
  * @version       $Rev: 1227 $
  * @author        $Author: DSB $
- * Maintained by: DSB (285), JanRei (23), Kyoya (31), StvN (2)
+ * Maintained by: DSB (286), JanRei (23), Kyoya (31), StvN (2)
  */
 $lang=array(
-'L_ACTION' => 'Aktion test',
+'L_ACTION' => 'Aktion',
 'L_ACTIVATED' => 'aktiviert',
 'L_ACTUALLY_INSERTED_RECORDS' => 'Es wurden bisher <b>%s</b> Datensätze erfolgreich eingetragen.',
 'L_ACTUALLY_INSERTED_RECORDS_OF' => 'Es wurden bisher <b>%s</b> von <b>%s</b> Datensätzen erfolgreich eingetragen.',
