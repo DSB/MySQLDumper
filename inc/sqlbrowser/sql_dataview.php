57c96a37cc84c98a0665ed835123aeddd7c11d68
squigulum <mm@ucall.com>
2018-03-27 22:09:49 +0200

    allow lowercase tablenames

--- inc/sqlbrowser/sql_dataview.php
+++ inc/sqlbrowser/sql_dataview.php
@@ -86,17 +86,20 @@ if ($sql_to_display_data == 0)
 }
 else
 {
+	// auch alle Tabellen-Namen werden lowercase! -> das kann zu Problemen fuehren
+	// siehe https://dev.mysql.com/doc/refman/5.7/en/identifier-case-sensitivity.html
 	$sql_temp=strtolower($sql['sql_statement']);
 	if (substr($sql_temp,0,7) == 'select ')
 	{
 		if (false !== strpos($sql_temp,' limit '))
 		{
-			// es wurde ein eigenes Lmit im Query angegeben - eigene Berechnung abbrechen
+			// es wurde ein eigenes Limit im Query angegeben - eigene Berechnung abbrechen
 			$numrowsabs=-1;
 		}
 		else
 		{
-			$sql_temp="SELECT count(*) as anzahl FROM (".$sql_temp.") as query;";
+			// anstatt sql_temp in lowerase hier das 'original' sql_statement verwenden
+			$sql_temp="SELECT count(*) as anzahl FROM (".$sql['sql_statement'].") as query;";
 			$res=@MSD_query($sql_temp,false);
 			if ($res)
 			{
